dc2a9ef732a82a77a1eac341f8316f037f7ae621
AgnesSteffi <agnessteffi525@gmail.com>
2024-08-18 20:19:42 +0530

    Initial commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Chicago', flag: 'germany.png', url: 'America/Chicago');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime() ;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Center(
+          child: SpinKitSpinningLines(
+          color: Colors.white,
+            size: 80.0,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/world_time.dart
@@ -0,0 +1,39 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+
+  late String location; //location name for the UI
+  late String time; //time in that location
+  late String flag; //url to an asset flag icon
+  late String url; //location url for api endpoint
+  bool? isDaytime; //true or false if day time or not
+
+  WorldTime({ required this.location,required this.flag,required this.url });
+
+  Future<void> getTime() async {
+
+    try {
+      //make the request
+      Response response = await get(Uri.parse('https://www.timeapi.io/api/Time/current/zone?timeZone=$url'));
+      Map data = jsonDecode(response.body);
+      print(data);
+
+      //get properties from data
+      String datetime = data['dateTime'] ?? 'Unknown Time';
+
+      //create a datetime object
+      DateTime now = DateTime.parse(datetime);
+
+      //set the time property
+      isDaytime = now.hour >= 6 && now.hour < 18;
+      time = DateFormat.jm().format(now);
+    }
+    catch (e) {
+      print('caught error: $e');
+      time = 'could not get the time';
+    }
+  }
+}
+
